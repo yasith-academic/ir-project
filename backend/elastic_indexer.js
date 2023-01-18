4ad207996adafa5049206cf4b66d4ff9c91c21ba
YasithD <yasith.18@cse.mrt.ac.lk>
2023-01-18 06:51:32 +0530

    create elastic search backend

---
+++ backend/elastic_indexer.js
@@ -0,0 +1,219 @@
+"use strict";
+
+require("array.prototype.flatmap").shim();
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://localhost:9200" });
+const song_data = require("../elasticsearch/song_data.json");
+
+async function run() {
+  await client.indices.create(
+    {
+      index: "songs",
+      body: {
+        settings: {
+          index: {
+            analysis: {
+              analyzer: {
+                sinhala_language_analyzer: {
+                  type: "custom",
+                  filter: [
+                    "song_stop_word_filter",
+                    "artist_stop_word_filter",
+                    "lyricist_stop_word_filter",
+                    "other_stop_word_filter",
+                    "sinhala_n_gram",
+                  ],
+                  tokenizer: "icu_tokenizer",
+                },
+                case_insensitive_analyzer: {
+                  filter: ["lowercase"],
+                  tokenizer: "standard",
+                },
+              },
+              filter: {
+                song_stop_word_filter: {
+                  type: "stop",
+                  stopwords: [
+                    "ගීත",
+                    "ගීතය",
+                    "ගී",
+                    "සිංදු",
+                    "සිංදූ",
+                    "සංගීතය",
+                    "සංගීතවත්",
+                  ],
+                },
+                artist_stop_word_filter: {
+                  type: "stop",
+                  stopwords: ["ගැයූ", "ගායනා", "ගීතවත්", "කිව්ව", "කීව"],
+                },
+                lyricist_stop_word_filter: {
+                  type: "stop",
+                  stopwords: ["රචනා", "රචිත", "ලියුව", "ලියූ"],
+                },
+                other_stop_word_filter: {
+                  type: "stop",
+                  stopwords: ["කල", "කරපු", "කෙරූ"],
+                },
+                sinhala_n_gram: {
+                  type: "edge_ngram",
+                  min_gram: 2,
+                  max_gram: 10,
+                },
+              },
+            },
+          },
+        },
+        mappings: {
+          properties: {
+            title: {
+              type: "text",
+              analyzer: "sinhala_language_analyzer",
+              fields: {
+                sinhala: {
+                  type: "text",
+                  analyzer: "sinhala_language_analyzer",
+                },
+                case_insensitive: {
+                  type: "text",
+                  analyzer: "case_insensitive_analyzer",
+                },
+              },
+            },
+            artist: {
+              type: "text",
+              analyzer: "case_insensitive_analyzer",
+              fields: {
+                case_insensitive: {
+                  type: "text",
+                  analyzer: "case_insensitive_analyzer",
+                },
+              },
+            },
+            genre: {
+              type: "keyword",
+            },
+            lyricist: {
+              type: "text",
+              analyzer: "case_insensitive_analyzer",
+              fields: {
+                case_insensitive: {
+                  type: "text",
+                  analyzer: "case_insensitive_analyzer",
+                },
+              },
+            },
+            music: {
+              type: "text",
+              analyzer: "case_insensitive_analyzer",
+              fields: {
+                case_insensitive: {
+                  type: "text",
+                  analyzer: "case_insensitive_analyzer",
+                },
+              },
+            },
+            key: {
+              type: "text",
+              analyzer: "case_insensitive_analyzer",
+              fields: {
+                case_insensitive: {
+                  type: "text",
+                  analyzer: "case_insensitive_analyzer",
+                },
+              },
+            },
+            beat: {
+              type: "keyword",
+            },
+            lyrics: {
+              type: "text",
+              analyzer: "sinhala_language_analyzer",
+              fields: {
+                sinhala: {
+                  type: "text",
+                  analyzer: "sinhala_language_analyzer",
+                },
+              },
+            },
+            metaphor: {
+              type: "text",
+              analyzer: "sinhala_language_analyzer",
+              fields: {
+                sinhala: {
+                  type: "text",
+                  analyzer: "sinhala_language_analyzer",
+                },
+              },
+            },
+            metaphor_interpretation: {
+              type: "text",
+              analyzer: "sinhala_language_analyzer",
+              fields: {
+                sinhala: {
+                  type: "text",
+                  analyzer: "sinhala_language_analyzer",
+                },
+              },
+            },
+            metaphor_source_domain: {
+              type: "text",
+              analyzer: "sinhala_language_analyzer",
+              fields: {
+                sinhala: {
+                  type: "text",
+                  analyzer: "sinhala_language_analyzer",
+                },
+              },
+            },
+            metaphor_target_domain: {
+              type: "text",
+              analyzer: "sinhala_language_analyzer",
+              fields: {
+                sinhala: {
+                  type: "text",
+                  analyzer: "sinhala_language_analyzer",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    { ignore: [400] }
+  );
+
+  const operations = song_data.flatMap((doc) => [
+    { index: { _index: "songs" } },
+    doc,
+  ]);
+
+  const bulkResponse = await client.bulk({ refresh: true, operations });
+
+  if (bulkResponse.errors) {
+    const erroredDocuments = [];
+    // The items array has the same order of the dataset we just indexed.
+    // The presence of the `error` key indicates that the operation
+    // that we did for the document has failed.
+    bulkResponse.items.forEach((action, i) => {
+      const operation = Object.keys(action)[0];
+      if (action[operation].error) {
+        erroredDocuments.push({
+          // If the status is 429 it means that you can retry the document,
+          // otherwise it's very likely a mapping error, and you should
+          // fix the document before to try it again.
+          status: action[operation].status,
+          error: action[operation].error,
+          operation: operations[i * 2],
+          document: operations[i * 2 + 1],
+        });
+      }
+    });
+    console.log(erroredDocuments);
+  }
+
+  const count = await client.count({ index: "songs" });
+  console.log(count);
+}
+
+run().catch(console.log);
---
+++ backend/index.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://localhost:9200" });
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/search", async (req, res) => {
+  const {
+    title,
+    artist,
+    genre,
+    lyricist,
+    music,
+    key,
+    beat,
+    lyrics,
+    metaphore,
+  } = req.body;
+
+  let conditions = [];
+  if (title) {
+    conditions.push({ match: { "title.sinhala": title } });
+    conditions.push({ match: { "title.case_insensitive": title } });
+  } else if (artist) {
+    conditions.push({ match: { "artist.case_insensitive": artist } });
+  } else if (genre) {
+    conditions.push({ match: { genre: genre } });
+  } else if (lyricist) {
+    conditions.push({ match: { "lyricist.case_insensitive": lyricist } });
+  } else if (music) {
+    conditions.push({ match: { "music.case_insensitive": music } });
+  } else if (key) {
+    conditions.push({ match: { "key.case_insensitive": key } });
+  } else if (beat) {
+    conditions.push({ match: { beat: beat } });
+  } else if (lyrics) {
+    conditions.push({ match: { "lyrics.sinhala": lyrics } });
+  } else if (metaphore) {
+    conditions.push({ match: { "metaphore.sinhala": metaphore } });
+  }
+
+  const result = await client.search({
+    index: "songs",
+    query: {
+      bool: {
+        must: conditions,
+      },
+    },
+  });
+
+  const output = result.hits.hits.map((hit) => ({
+    score: hit._score,
+    title: hit._source.title,
+    artist: hit._source.artist,
+    genre: hit._source.genre,
+    lyricist: hit._source.lyricist,
+    music: hit._source.music,
+    key: hit._source.key,
+    beat: hit._source.beat,
+    lyrics: hit._source.lyrics,
+    metaphore: hit._source.metaphore,
+    metaphor_interpretation: hit._source.metaphor_interpretation,
+    metaphor_source_domain: hit._source.metaphor_source_domain,
+    metaphor_target_domain: hit._source.metaphor_target_domain,
+  }));
+
+  console.log("GET /search");
+  res.json(output);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
